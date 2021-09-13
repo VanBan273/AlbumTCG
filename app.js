b797aff6d989bf1c7ca755465473619897170a86
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-09-13 13:23:26 +0200

    Bases
    Bases plantadas y fuente

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ const app = express();
 require("./config")(app);
 
 // default value for title local
-const projectName = "ironlauncherBootstrap";
+const projectName = "AlbumTCG";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.title = `${capitalized(projectName)} created by VanBan `;
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
