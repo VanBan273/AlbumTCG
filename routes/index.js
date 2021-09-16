aa14054a1d4efb2f9b972ebe7e57deed5a8f53e3
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-09-16 10:31:59 +0200

    add
    addda

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 module.exports = (app) => {
 
   app.use("/auth", require('./auth'))
-  app.use("/", require('./cards.routes'))
+  app.use("/", require('./cards.routes.js'))
   app.use("/", require('./base.routes.js'))
   
   }  
